e67193560a01ac08f1b9526a5e93590e304d9bd6
M <mai.indigo@hotmail.com>
2015-11-14 15:00:30 +0700

    Sound Effect Successful

--- app/src/main/java/com/indigocorp/mai/butraffic/MainActivity.java
+++ app/src/main/java/com/indigocorp/mai/butraffic/MainActivity.java
@@ -1,13 +1,48 @@
 package com.indigocorp.mai.butraffic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    // ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutmeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controller
+
+        buttonController();
+
+    } // Main Method
+
+    private void buttonController() {
+        aboutmeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer buttonPlayer = MediaPlayer.create(getBaseContext(), R.raw.lion);
+                buttonPlayer.start();
+                // Intent to WebView
+                
+            }//Event
+        });
     }
-}
+
+    private void bindWidget() {
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutmeButton = (Button) findViewById(R.id.button);
+    }
+
+}   // Main Class
+
